825f31b00231acd4b502a72ebf50e7af08d14aa2
harekuto <hillerjojojo@gmail.com>
2026-08-22 07:21:54 -0400

    Add WillLock app Gradle build

---
+++ willlock-ci/app/build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.willlock.app"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.willlock.app"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 2
+        versionName = "1.0.1"
+    }
+
+    buildTypes {
+        release { isMinifyEnabled = false }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions { jvmTarget = "17" }
+}
